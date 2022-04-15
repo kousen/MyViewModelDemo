affc23af0b4a76a27d18e07002246ef886380bba
Ken Kousen <ken.kousen@kousenit.com>
2022-04-14 22:43:42 -0400

    Remove unused import and clean up code

--- app/src/main/java/com/oreilly/myviewmodeldemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/oreilly/myviewmodeldemo/ui/main/MainFragment.kt
@@ -2,14 +2,12 @@ package com.oreilly.myviewmodeldemo.ui.main
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.oreilly.myviewmodeldemo.R
 import com.oreilly.myviewmodeldemo.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
