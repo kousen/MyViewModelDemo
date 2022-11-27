70d6dc0069b2431d6b507ad82c8c94623fc0f8cc
Ken Kousen <ken.kousen@kousenit.com>
2022-11-27 16:22:32 -0500

    Upgrade dependencies

--- app/src/main/java/com/oreilly/myviewmodeldemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/oreilly/myviewmodeldemo/ui/main/MainFragment.kt
@@ -2,9 +2,7 @@ package com.oreilly.myviewmodeldemo.ui.main
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
